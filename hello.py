ae0b11007e82622731fdd44c0f72ff2dde1f3dce
MahendharN <mahendhar.n@amagi.com>
2023-11-23 12:22:01 +0530

    test

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print("Hi hello")
+import os
+
+# Get information about the pull request from environment variables
+base_branch = os.environ.get('GITHUB_BASE_REF', 'unknown_base_branch')
+feature_branch = os.environ.get('GITHUB_HEAD_REF', 'unknown_feature_branch')
+
+# Print information
+print(f'This pull request is merging changes from {feature_branch} to {base_branch}.')
