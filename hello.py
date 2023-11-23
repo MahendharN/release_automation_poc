c3715f996f95199c7dab05a2e52b3511bf5b97ec
MahendharN <mahendhar.n@amagi.com>
2023-11-23 12:24:58 +0530

    test

--- hello.py
+++ hello.py
@@ -1,8 +1,28 @@
 import os
+import subprocess
 
-# Get information about the pull request from environment variables
-base_branch = os.environ.get('GITHUB_BASE_REF', 'unknown_base_branch')
-feature_branch = os.environ.get('GITHUB_HEAD_REF', 'unknown_feature_branch')
+# Get the name of the head branch
+head_branch = os.environ.get('GITHUB_HEAD_REF', 'unknown_head_branch')
 
-# Print information
-print(f'This pull request is merging changes from {feature_branch} to {base_branch}.')
+# Find a branch with "rc" in its name
+rc_branch = None
+with subprocess.Popen(['gh', 'repo', 'view', '--json', 'default_branch', '-q', '.default_branch'], stdout=subprocess.PIPE) as process:
+    default_branch, _ = process.communicate()
+    default_branch = default_branch.decode().strip()
+
+with subprocess.Popen(['gh', 'repo', 'view', '--json', 'branches', '-q', '.branches[*].name'], stdout=subprocess.PIPE) as process:
+    branches, _ = process.communicate()
+    branches = branches.decode().strip().split()
+
+for branch in branches:
+    if 'rc' in branch:
+        rc_branch = branch
+        break
+
+# Create a pull request from the head branch to the branch with "rc" in its name
+if rc_branch:
+    with subprocess.Popen(['gh', 'pr', 'create', '--base', default_branch, '--head', head_branch, '--title', f'Merge {head_branch} to {rc_branch}', '--body', f'This PR includes changes from {head_branch} to {rc_branch}.'], stdout=subprocess.PIPE) as process:
+        output, _ = process.communicate()
+        print(output.decode())
+else:
+    print(f'No branch found with "rc" in its name.')
