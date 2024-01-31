581fb0d942697f787dd2fd93d312fbaa4a5e7700
MahendharN <mahendhar.n@amagi.com>
2024-01-31 11:15:48 +0530

    sdjdbn

--- .github/scripts/pr_description_check.py
+++ .github/scripts/pr_description_check.py
@@ -18,21 +18,27 @@ if re.match(title_pattern, title):
 # Extract PR description
 description = event_payload['pull_request']['body']
 if description is None:
-    description=""
+    print("PR description is not valid. Please ensure it follows the required format.")
+    exit(1)
 
-print(description)
-
-description = description.lower()
+description = "".join(description.lower().split("\n"))
 
 # Define a pattern for required fields in a text string using the 'compulsory_pattern' variable
-compulsory_pattern = "title:.*\ndescription:.*\njira:.*\ntest report:.*"
+compulsory_pattern = "title:.*description:.*jira:.*test report:.*"
+incorrect_compulsary_pattern = "title:.*|description:.*|jira:.*|test report:.*"
 optional_pattern = "dependencies:.*|limitations:.*|deprecated features:.*"
 
 
 optional_pattern_found = re.search(optional_pattern,description)
 compulsory_pattern_found = re.search(compulsory_pattern,description)
+incorrect_compulsary_pattern_found = re.search(incorrect_compulsary_pattern,description)
 
-if compulsory_pattern_found or optional_pattern_found:
+if compulsory_pattern_found:
+    print("PR description is valid.")
+elif not compulsory_pattern_found and incorrect_compulsary_pattern_found:
+    print("PR description is not valid. Please ensure it follows the required format.")
+    exit(1)
+elif optional_pattern_found:
     print("PR description is valid.")
 else:
     print("PR description is not valid. Please ensure it follows the required format.")
