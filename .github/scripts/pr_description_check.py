f6a6edaaa38cb585498da441924d49c022144815
MahendharN <mahendhar.n@amagi.com>
2024-01-31 11:24:37 +0530

    sdjdbn

--- .github/scripts/pr_description_check.py
+++ .github/scripts/pr_description_check.py
@@ -21,21 +21,21 @@ if description is None:
     print("PR description is not valid. Please ensure it follows the required format.")
     exit(1)
 
-description = "".join(description.lower().split("\n"))
+processed_desc = "".join(description.lower().split("\n"))
 
 # Define a pattern for required fields in a text string using the 'compulsory_pattern' variable
 compulsory_pattern = "title:.*description:.*jira:.*test report:.*"
-incorrect_compulsary_pattern = "title:.*|description:.*|jira:.*|test report:.*"
+keywords = ["Jira", "Title", "Test Report", "Description"]
+incorrect_compulsary_pattern = r"\b(?:" + "|".join(keywords) + r")\b"
 optional_pattern = "dependencies:.*|limitations:.*|deprecated features:.*"
 
 
-optional_pattern_found = re.search(optional_pattern,description)
-compulsory_pattern_found = re.search(compulsory_pattern,description)
-incorrect_compulsary_pattern_found = re.search(incorrect_compulsary_pattern,description)
+optional_pattern_found = re.search(optional_pattern,processed_desc)
+compulsory_pattern_found = re.search(compulsory_pattern,processed_desc)
 
 if compulsory_pattern_found:
     print("PR description is valid.")
-elif not compulsory_pattern_found and incorrect_compulsary_pattern_found:
+elif not compulsory_pattern_found and any(keyword in description for keyword in keywords):
     print("PR description is not valid. Please ensure it follows the required format.")
     exit(1)
 elif optional_pattern_found:
@@ -43,3 +43,4 @@ elif optional_pattern_found:
 else:
     print("PR description is not valid. Please ensure it follows the required format.")
     exit(1)
+    
