48b352026664332c24d2758b7e07142e19ea06e7
MahendharN <mahendhar.n@amagi.com>
2024-02-21 16:47:19 +0530

    kjnsjkdnjk

--- .github/scripts/create_release_notes.py
+++ .github/scripts/create_release_notes.py
@@ -12,7 +12,7 @@ TAGLIST_FILE_PATH = "./taglist.yaml"
 AUTHOR = "Blizzard"
 
 class ReleaseNotesGenerator:
-    def __init__(self,base_branch,git_repo,git_token,present_tag):
+    def __init__(self, base_branch, git_repo, git_token, present_tag):
         self.base_branch = base_branch
         self.last_tag = self.__get_last_tag_from_taglist()
         if self.last_tag is None:
@@ -23,7 +23,15 @@ class ReleaseNotesGenerator:
         self.present_tag = present_tag
 
         if self.last_tag == self.present_tag:
-            print(f"Last tag and Present tag are same. Last tag {self.last_tag} and Present Tag {self.present_tag}")
+            print(f"Last tag and Present tag are the same. Last tag: {self.last_tag}, Present Tag: {self.present_tag}")
+
+        # Log initialization details
+        print(f"Initialized ReleaseNotesGenerator with the following parameters:")
+        print(f"Base branch: {self.base_branch}")
+        print(f"Last tag: {self.last_tag}")
+        print(f"Git repository: {self.git_repo}")
+        print(f"Git token: {self.git_token}")
+        print(f"Present tag: {self.present_tag}")
 
     def __get_last_tag_from_taglist(self):
         """
@@ -43,10 +51,11 @@ class ReleaseNotesGenerator:
 
     def __get_pr_list_from_github_api(self):
         """
-        Get list of PR using generate release notes Github API
+        Get a list of PRs using the GitHub API for generating release notes.
         """
-        # Get list of PR using generate release notes Github API
-        pr_list_res = requests.post(
+        try:
+            # Make a POST request to the GitHub API
+            pr_list_res = requests.post(
                 f"https://api.github.com/repos/{self.git_repo}/releases/generate-notes",
                 headers={
                     "Authorization": f"Bearer {self.git_token}",
@@ -61,25 +70,36 @@ class ReleaseNotesGenerator:
                 },
             ).json()
         
-        lines = pr_list_res["body"].splitlines()
-        pr_info_list = []
-        
-        # Extract PR body and title
-        for line in lines:
-            if line.startswith('* '):
-                res = line.rsplit('/',1)
-                pr_info = requests.get(
-                f"https://api.github.com/repos/{self.git_repo}/pulls/{res[1]}",
-                headers={
-                    "Authorization": f"Bearer {self.git_token}",
-                    "Accept": "application/vnd.github.v3+json",
-                },
-                ).json()
-                pr_info_list.append(pr_info)
+        # Extract PR information from the response body
+            lines = pr_list_res["body"].splitlines()
+            pr_info_list = []
 
-        return pr_info_list
+            for line in lines:
+                if line.startswith("* "):
+                    # Extract PR number from the line
+                    pr_number = line.rsplit("/", 1)[1]
+
+                    # Fetch detailed PR information
+                    pr_info = requests.get(
+                        f"https://api.github.com/repos/{self.git_repo}/pulls/{pr_number}",
+                        headers={
+                            "Authorization": f"Bearer {self.git_token}",
+                            "Accept": "application/vnd.github.v3+json",
+                        },
+                    ).json()
+
+                    pr_info_list.append(pr_info)
+
+            return pr_info_list
+        except Exception as exc:
+            print("Exception occurred while fetching PR information from GitHub API:")
+            print(exc)
+            exit(1)
     
-    def __get_list_of_description(self,pr_info_list):
+    def __get_list_of_description(self, pr_info_list):
+        """
+        Get a list of PR descriptions from the PR information list.
+        """
         description_list = []
         for pr_info in pr_info_list:
             if "(Build)" in pr_info.get("title"):
@@ -88,7 +108,7 @@ class ReleaseNotesGenerator:
             try:
                 description = yaml1.safe_load(description)
             except Exception as e:
-                print(f"Error while generating yaml for {pr_info.get('url')}. Error {e}")
+                print(f"Error while generating YAML for {pr_info.get('url')}. Error: {e}")
                 description = None
             if not description:
                 print(f"Description of {pr_info.get('url')} is empty")
@@ -98,7 +118,7 @@ class ReleaseNotesGenerator:
     
     def generate_release_notes(self):
         pr_list = self.__get_pr_list_from_github_api()
-        if pr_list is None or len(pr_list) == 0:
+        if not pr_list:
             print("No PR found to generate release notes")
             sys.exit(0)
         description_list = self.__get_list_of_description(pr_list)
@@ -129,12 +149,12 @@ class ReleaseNotesGenerator:
             try:
                 yaml.dump(updated_data, stream)
             except Exception as e:
-                print(f"Unable to dump YAML data in path {TAGLIST_FILE_PATH} , data {updated_data}, Error {e}")
+                print(f"Unable to dump YAML data in path {TAGLIST_FILE_PATH}, data {updated_data}, Error {e}")
                 return
         print(f"Tag '{self.present_tag}' added successfully to path {TAGLIST_FILE_PATH}!")
 
 
-    def __get_dict_to_update_in_build_notes(self,description_list):
+    def __get_dict_to_update_in_build_notes(self, description_list):
         build_dict = {}
         build_dict["Tag"] = self.present_tag
         current_datetime = datetime.now()
@@ -145,9 +165,9 @@ class ReleaseNotesGenerator:
         dependencies = []
         limitations = []
         for description in description_list:
-            for tickets in description.get("Tickets",[]):
+            for tickets in description.get("Tickets", []):
                 desc = tickets.get("Description")
-                ticket = tickets.get("JiraID","")
+                ticket = tickets.get("JiraID", "")
                 print(tickets)
                 if desc is None:
                     desc = ""
@@ -155,7 +175,7 @@ class ReleaseNotesGenerator:
                     jira_dict[ticket] = ""
                 elif ticket in jira_dict:
                     similarity = fuzz.ratio(desc, jira_dict[ticket])
-                    print("sim",similarity)
+                    print("sim", similarity)
                     if similarity < 70:
                         jira_dict[ticket] = f"{jira_dict[ticket]}. {desc}"
                     else:
@@ -163,21 +183,22 @@ class ReleaseNotesGenerator:
                 else:
                     jira_dict[ticket] = desc
             print(jira_dict)
-            dependencies += description.get("Dependencies",[])
-            deprecated_features += description.get("Deprecated Features",[])
-            limitations += description.get("Limitations",[])
-        jira_list = [{"JiraID":k,"description":v} for k,v in jira_dict.items()]
+            dependencies += description.get("Dependencies", [])
+            deprecated_features += description.get("Deprecated Features", [])
+            limitations += description.get("Limitations", [])
+        jira_list = [{"JiraID": k, "description": v} for k, v in jira_dict.items()]
         if len(jira_list) != 0:
             build_dict["Changes"] = jira_list
-        if len(dependencies)!=0:
+        if len(dependencies) != 0:
             build_dict["Dependencies"] = list(set(dependencies))
-        if len(limitations) !=0:
+        if len(limitations) != 0:
             build_dict["Limitations"] = list(set(limitations))
         if len(deprecated_features) != 0:
             build_dict["Deprecated Features"] = list(set(deprecated_features))
-        return {"BuildNotes":build_dict}
+        return {"BuildNotes": build_dict}
+
     
-    def __update_into_file(self,data):
+    def __update_into_file(self, data):
         yaml = YAML()
         yaml.indent(sequence=4, offset=2)
         yaml.default_flow_style = False
@@ -186,7 +207,7 @@ class ReleaseNotesGenerator:
             try:
                 yaml.dump(data, file)
             except Exception as e:
-                print(f"Unable to dump YAML data in path {BUILD_NOTES_FILE_PATH} , data {data}, Error {e}")
+                print(f"Unable to dump YAML data in path {BUILD_NOTES_FILE_PATH}, data {data}, Error {e}")
                 return
         print(f"{BUILD_NOTES_FILE_PATH} created successfully.")
 
@@ -196,9 +217,9 @@ if __name__ == "__main__":
     git_token = sys.argv[3]
     present_tag = sys.argv[4]
     if base_branch != "develop" and (not base_branch.endswith(".x")) and (not base_branch.startswith("rc")):
-        print ("This workflow only applicable for .x, develop and rc branches")
+        print("This workflow is only applicable for .x, develop, and rc branches")
         sys.exit(1)
 
-    release_notes_obj = ReleaseNotesGenerator(base_branch,git_repo,git_token,present_tag)
+    release_notes_obj = ReleaseNotesGenerator(base_branch, git_repo, git_token, present_tag)
     release_notes_obj.generate_release_notes()
     release_notes_obj.generate_taglist()
