040f974622f1f079f20c474c42b0a5e63fc8f102
MahendharN <mahendhar.n@amagi.com>
2024-02-22 14:46:26 +0530

    skmd

--- .github/scripts/revert_build_notes_before_merge.sh
+++ .github/scripts/revert_build_notes_before_merge.sh
@@ -1,6 +1,8 @@
 git config --local user.email "actions@github.com"
 git config --local user.name "GitHub Actions"
-git checkout develop
+BASE_BRANCH=$1
+HEAD_BRANCH=$2
+git checkout $BASE_BRANCH
 cp build_notes.yaml build_notes1.yaml
 cp taglist.yaml taglist1.yaml
 git checkout $HEAD_BRANCH
@@ -8,5 +10,5 @@ mv build_notes1.yaml build_notes.yaml
 mv taglist1.yaml taglist.yaml
 git add build_notes.yaml taglist.yaml
 git commit -m "ci(Unchange Build Notes): Build Notes and taglist changes reverted"
-git push origin ${{ github.head_ref }}
+git push origin $HEAD_BRANCH
 
